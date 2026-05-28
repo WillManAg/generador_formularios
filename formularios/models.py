3eb9e335811a9810092b8780940e7f076ca42991
WillManAg <d3v.william@gmail.com>
2026-05-28 20:56:59 +0200

    Armando la base de datos que almacene los datos a introducir

--- formularios/models.py
+++ formularios/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class PlantillaDocumento(models.Model):
+
+    nombre = models.CharField(max_length=150 verbose_name="Nombre del Formulario")
+
+    descripcion = models.TextField(blank=True, null=True, verbose_name="Descripción")
+
+    archivo_word = models.CharField(max_length=255, verbose_name="Nombre del archivo Word (.docx)")
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Plantilla de Documento"
+        verbose_name_plural = "Plantillas de Documentos"
+        
+class CampoFormulario(models.Model):
+
+    plantilla = models.ForeignKey(
+        PlantillaDocumento,
+        on_delete=models.CASCADE,
+        related_name="campos"
+    )
+
+    nombre_variable = models.CharField(
+        max_length=50,
+        verbose_name="Variable en el Word (ej: correo_electrónic)"
+    )
